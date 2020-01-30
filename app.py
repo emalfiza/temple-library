20793965f45227e0b81716e67d8a0259802e363f
Emal Fiza <emal_fiza@hotmail.com>
2020-01-30 16:22:03 +0000

    templates folder added for parents and chillds files

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo  # To get Flask interacting with MongoDB #
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'temple_library'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
-@app.route("/")
-def hello():
-    return "Hello World ...again"
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/book')
+def book():
+    return render_template("book.html", book=mongo.db.book.find())
 
 
 if __name__ == "__main__":
